bcdb3dcf647eb39a882852f4fab48c2c32cb0232
s31824 <s31824@pjwstk.edu.pl>
2026-03-14 19:57:14 +0100

    Fix database connection to use env variables and SSL

--- server/util/database.js
+++ server/util/database.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'f1database',
-    password: '',
-    port: 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool.promise();
